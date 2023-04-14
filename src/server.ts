8d7f5d33730c38de3bac0811e60a082d2f9d58e1
Willian Mascarenhas <masca.will@gmail.com>
2023-04-14 16:22:04 -0300

    feat: todas as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
 import { startDatabase } from "./database";
+import "dotenv/config";
 
-const appPort = 3000 || process.env.APP_PORT;
+const appPort = Number(process.env.APP_PORT) || 3000;
 
 const server = (port: number) =>
   app.listen(port, async () => {
